372c241559e2760f0d8799f8d0a57e50f04f5b86
breadf1sh <32587132+ddukbg@users.noreply.github.com>
2018-06-09 23:13:16 +0900

    DB등록시 PRIMARY KEY에러시 예외 처리 추가
    DB등록시 PRIMARY KEY에러시 예외 처리 추가

--- 크롤러/ptest.py
+++ 크롤러/ptest.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import pymysql
-
+#서버 설정 
 conn = pymysql.connect(
     user = 'sjuackr18',
     passwd = 'sejonguniv2018',
@@ -16,7 +16,8 @@ kalinfor=[]
 foodsg=[]
 
 page=1
-for page in range(1,678,1):
+for page in range(1,678,1):#시작페이지와 끝페이지
+    print('%d 페이지 시작',page)
     req = urllib.request.Request("http://47kg.kr/contents/caldic/update_list.php?&page="+ str(page), headers={'User-Agent': 'Mozilla/5.0'})
     response = urllib.request.urlopen(req)
     text = response.read()
@@ -26,7 +27,6 @@ for page in range(1,678,1):
 
     menu = soup.find_all('a')
     kal = soup.find_all('td')
-    #price = soup.find_all('div',{'class':'td price'})
     ##리스트 menu와 price에 쓰레기값 제거
     for n in menu:
         i = menu.index(n)
@@ -58,16 +58,27 @@ for page in range(1,678,1):
         foodlist3 += kal[i] + "\n"
         foodsg.append(kal[i])
     #print(foodlist3)
+        
+    #FOOD DB내의 음식명이 PRIMARY KEY이기에 발생하는 에러를 예외처리함
+    res=0
 
-    #print(foodname)
-    #print(kalinfor)
-    #print(foodsg)
-
-    for i in range(0,14):
-        cur.execute("INSERT INTO FOOD(menu,kcal,cafeteria,amount,unit) VALUES (%s,%s,%s,%s,%s)",(foodname[i],kalinfor[i],'기타',foodsg[i],foodsg[i]))
+       
+    try:
+        for i in range(0,14):
+            cur.execute("INSERT INTO FOOD(menu,kcal,cafeteria,amount,unit) VALUES (%s,%s,%s,%s,%s)",(foodname[i],kalinfor[i],'기타',foodsg[i],foodsg[i]))
+    except pymysql.IntegrityError:
+        res = 1    
+    except IntegrityError as e:
+        if not e[0] == 1062:
+            raise
+        else:
+            print(e, datetime.datetime.now())
+            traceback.print_exc()
+            res = 3
     #변경사항 커밋
-    conn.commit()    
-       # print(foodname[i],kalinfor[i],foodsg[i])
+    conn.commit()
+
+    
     foodname=[]
     kalinfor=[]
     foodsg=[]
